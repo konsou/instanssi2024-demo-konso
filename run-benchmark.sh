0a38fb763a5ac5b271dab78a493ea11ee77646ba
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-11 21:22:47 +0300

    Fix benchmark script

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -15,4 +15,4 @@ fi
 sudo Xvfb :99 & # Start Xvfb on display 99
 export DISPLAY=:99.0
 
-processing-java --sketch=$(pwd) --run $RUNTIME_SECONDS $FPS_CAP
+processing-java --sketch=$(pwd) --run auto-benchmark $RUNTIME_SECONDS $FPS_CAP
