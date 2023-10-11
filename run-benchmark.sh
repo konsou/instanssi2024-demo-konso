12fe146d6a3fadfd6c3a5916182a8ace8aafa478
stress-erkki <tomi.javanainen+stress-erkki@gmail.com>
2023-10-11 21:45:22 +0300

    Add commit info to benchmark data. Auto-commit and push benchmark results.

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -3,6 +3,7 @@
 # Xvfb should be installed for pseudo-display
 RUNTIME_SECONDS=60
 FPS_CAP=999
+OUTPUT_FILENAME=perf-measurements.log
 
 # Check if the lock file exists before trying to remove it
 if [ -e /tmp/.X99-lock ]; then
@@ -13,7 +14,13 @@ fi
 sudo Xvfb :99 & # Start Xvfb on display 99
 export DISPLAY=:99.0
 
-processing-java --sketch=$(pwd) --run auto-benchmark $RUNTIME_SECONDS $FPS_CAP
+echo "--------------------------" >> $OUTPUT_FILENAME
+git log -1 >> $OUTPUT_FILENAME
+processing-java --sketch=$(pwd) --run auto-benchmark $RUNTIME_SECONDS $FPS_CAP | tee -a $OUTPUT_FILENAME
+
+git add $OUTPUT_FILENAME
+git commit -m "Add benchmark info"
+git push
 
 # Clean up Xvfb
 sudo pkill -f "Xvfb :99"
