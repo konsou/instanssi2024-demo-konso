9ebe8ebe117c7e5cff07871e4a909981016e22cd
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-11 21:20:55 +0300

    Tweak benchmark script

--- run-benchmark.sh
+++ run-benchmark.sh
@@ -1,8 +1,16 @@
 #!/usr/bin/env bash
+set -x  # Turn on command echoing
+
 # processing-java should be in PATH
 # Xvfb should be installed for pseudo-display
 RUNTIME_SECONDS=60
 FPS_CAP=999
+
+# Check if the lock file exists before trying to remove it
+if [ -e /tmp/.X99-lock ]; then
+  echo "Cleaning up Xvfb display 99"
+  sudo rm /tmp/.X99-lock
+fi
 
 sudo Xvfb :99 & # Start Xvfb on display 99
 export DISPLAY=:99.0
