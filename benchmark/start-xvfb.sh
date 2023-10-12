1459c9d3baa1909bb1657f19d6cfb04dfa72b8e2
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-12 16:40:38 +0300

    Service file and startup script for running xvfb as a service

---
+++ benchmark/start-xvfb.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+DOTENV_FILE="${SCRIPT_DIR}/.env"
+
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root" >&2
+   exit 1
+fi
+
+# Load configurations from .env
+export "$(grep -v '^#' "${DOTENV_FILE}" | xargs)"
+
+if [ -z "${XVFB_DISPLAY_NUM}" ]; then
+    echo "Error: XVFB_DISPLAY_NUM is not set in ${DOTENV_FILE}." >&2
+    exit 1
+fi
+
+/usr/bin/Xvfb "${XVFB_DISPLAY_NUM}"
