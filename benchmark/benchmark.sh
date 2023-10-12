32832a6273ef6b0d4d55bb54ccbb3d864abc3f8a
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-13 01:30:52 +0300

    Run benchmark with maven

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -81,8 +81,8 @@ if [ "${PIPESTATUS[0]}" -ne 0 ]; then
     exit 1
 fi
 
-echo "Load Average before benchmark: $load_avg" | tee -a "${OUTPUT_FILENAME}"
-java -classpath "${PROJECT_ROOT}/lib/core.jar:${PROJECT_ROOT}/target/classes/" Instanssi2024DemoKonso auto-benchmark $RUNTIME_SECONDS $FPS_CAP 2>&1 | tee -a "${OUTPUT_FILENAME}"
+echo "Load averages before benchmark: $load_avg" | tee -a "${OUTPUT_FILENAME}"
+(cd "${PROJECT_ROOT}" && mvn exec:java -Dexec.args="--benchmark --runtime $RUNTIME_SECONDS --fps-cap $FPS_CAP") 2>&1 | tee -a "${OUTPUT_FILENAME}"
 # Check the exit status of the java command, not the tee
 if [ "${PIPESTATUS[0]}" -ne 0 ]; then
     echo "Running benchmark failed" | tee -a "${OUTPUT_FILENAME}"
