7f5cf39c94754812c14f603af9dc3954f6d06a4b
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-12 15:45:57 +0300

    Move benchmark stuff to own folder

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -21,9 +21,11 @@
 
 RUNTIME_SECONDS=60
 FPS_CAP=999
-OUTPUT_FILENAME=perf-measurements.log
-BASELINE_FILENAME=perf-baseline.log
-GIT_REPO=git@github.com:konsou/instanssi2024DemoKonso.git
+SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
+RESULT_DIR="${SCRIPT_DIR}/results"
+PROJECT_ROOT="${SCRIPT_DIR}/.."
+OUTPUT_FILENAME="${RESULT_DIR}/benchmark.log"
+BASELINE_FILENAME="${RESULT_DIR}/benchmark-baseline.log"
 LOCK_FILE="/tmp/konso-demo-benchmark.lock"
 
 SKIP_LOCK=${1:-"false"}
@@ -42,7 +44,7 @@ load_avg=$(uptime | awk -F'[a-z]:' '{ print $2 }')
 
 # Compile
 echo "Compiling"
-javac -classpath lib/core.jar -d out/ src/Instanssi2024DemoKonso.java
+javac -classpath "${PROJECT_ROOT}/lib/core.jar" -d "${PROJECT_ROOT}/out/" "${PROJECT_ROOT}/src/Instanssi2024DemoKonso.java"
 
 # Check if the lock file exists before trying to remove it
 if [ -e /tmp/.X99-lock ]; then
@@ -60,27 +62,27 @@ parse_frame_count() {
 }
 
 # Run the benchmark
-echo "--------------------------" >> $OUTPUT_FILENAME
-git log -1 >> $OUTPUT_FILENAME
-echo "Load Average before benchmark: $load_avg" | tee -a $OUTPUT_FILENAME
-java -classpath out/:lib/core.jar Instanssi2024DemoKonso auto-benchmark $RUNTIME_SECONDS $FPS_CAP | tee -a $OUTPUT_FILENAME
+echo "--------------------------" >> "${OUTPUT_FILENAME}"
+git log -1 >> "${OUTPUT_FILENAME}"
+echo "Load Average before benchmark: $load_avg" | tee -a "${OUTPUT_FILENAME}"
+java -classpath out/:lib/core.jar Instanssi2024DemoKonso auto-benchmark $RUNTIME_SECONDS $FPS_CAP | tee -a "${OUTPUT_FILENAME}"
 
 # Parse the frame counts
-baseline_frames=$(parse_frame_count $BASELINE_FILENAME)
-current_frames=$(parse_frame_count $OUTPUT_FILENAME)
+baseline_frames=$(parse_frame_count "${BASELINE_FILENAME}")
+current_frames=$(parse_frame_count "${OUTPUT_FILENAME}")
 
 # Calculate the difference and append to the log
 difference=$((current_frames - baseline_frames))
 percentage_change=$(echo "scale=2; ($current_frames - $baseline_frames) / $baseline_frames * 100" | bc)
-echo "Difference from baseline: $difference frames ($percentage_change%)" | tee -a $OUTPUT_FILENAME
+echo "Difference from baseline: $difference frames ($percentage_change%)" | tee -a "${OUTPUT_FILENAME}"
 
 # Push results
-git add $OUTPUT_FILENAME
+git add "${OUTPUT_FILENAME}"
 git commit -m "Add benchmark info"
-git push $GIT_REPO
+git push
 
 # Clean up Xvfb
-sudo ./kill_xvfb.sh
+sudo "${SCRIPT_DIR}/kill_xvfb.sh"
 
 # Only remove lockfile if not skipping lock checks
 if [ "${SKIP_LOCK}" != "skip-lock" ]; then
