130a56b12d18565574f96c461cea3e796b2ec36f
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-13 23:24:33 +0300

    Refactor, send results to save_result.py

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -14,8 +14,8 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 DOTENV_FILE="${SCRIPT_DIR}/.env"
 RESULT_DIR="${SCRIPT_DIR}/results"
 PROJECT_ROOT="${SCRIPT_DIR%/*}"
-BENCHMARK_RESULTS="${RESULT_DIR}/benchmark.log"
-BASELINE_FILENAME="${RESULT_DIR}/benchmark-baseline.log"
+BENCHMARK_RESULTS="${RESULT_DIR}/benchmark.jsonl"
+BASELINE_FILENAME="${RESULT_DIR}/benchmark-baseline.jsonl"
 DETAILED_LOG_DIR="${PROJECT_ROOT}/log"
 DETAILED_LOG="${DETAILED_LOG_DIR}/benchmark-full.log"
 LOCK_FILE="/tmp/konso-demo-benchmark.lock"
@@ -24,6 +24,13 @@ source "${SCRIPT_DIR}/dotenv.sh"
 source "${SCRIPT_DIR}/discord.sh"
 
 mkdir -p "${DETAILED_LOG_DIR}" || { echo "Failed to create log dir ${DETAILED_LOG_DIR}" >&2; exit 1; }
+
+log_with_timestamp() {
+    local timestamp
+    timestamp=$(date +'%Y-%m-%d %H:%M:%S %z')
+    echo "$1"
+    echo "${timestamp} | $1" >> "${DETAILED_LOG}"
+}
 
 { load_venv "${DOTENV_FILE}" ; } >> "${DETAILED_LOG}" 2>&1 || exit 1
 
@@ -55,15 +62,13 @@ touch "${LOCK_FILE}"
 load_avg=$(uptime | awk -F'[a-z]:' '{ print $2 }')
 
 parse_frame_count() {
-  local log_filename=$1
-  # Get the last occurrence of "Frame count (draw):" from the file
-  grep "Frame count (draw):" "$log_filename" | tail -n 1 | awk '{print $4}'
+  # Get the last occurrence of "Frame count (draw):" from the text
+  echo "$1" | grep "Frame count (draw):" | tail -n 1 | awk '{print $4}'
 }
 
 parse_average_fps() {
-  local log_filename=$1
-  # Get the last occurrence of "Average FPS (draw):" from the file
-  grep "Average FPS (draw):" "$log_filename" | tail -n 1 | awk '{print $4}'
+  # Get the last occurrence of "Average FPS (draw):" from the text
+  echo "$1" | grep "Average FPS (draw):" | tail -n 1 | awk '{print $4}'
 }
 
 remove_lockfile() {
@@ -73,44 +78,71 @@ remove_lockfile() {
   fi
 }
 
-# Run the benchmark
-echo "--------------------------" >> "${BENCHMARK_RESULTS}"
-echo "Benchmark started at $(date +'%Y-%m-%d %H:%M')" >> "${BENCHMARK_RESULTS}"
-git log -1 >> "${BENCHMARK_RESULTS}"
+# 1. Gather available info before running the benchmark to result array
+declare -A results_array
 
-# Compile
-echo "Building"
-(cd "${PROJECT_ROOT}" && mvn install:install-file "-Dfile=lib\core.jar" -DgroupId="processing.core" -DartifactId=core -Dversion="1.0" -Dpackaging=jar) || echo "Error adding core.jar to maven" | tee -a "${BENCHMARK_RESULTS}"
-(cd "${PROJECT_ROOT}" && mvn compile) || echo "Error compiling" | tee -a "${BENCHMARK_RESULTS}"
-# Check the exit status of the java command, not the tee
-if [ "${PIPESTATUS[0]}" -ne 0 ]; then
-    echo "Build failed" | tee -a "${BENCHMARK_RESULTS}"
+# Capture current commit details
+results_array["timestamp"]=$(date +'%Y-%m-%d %H:%M:%S %z')
+results_array["commit_hash"]=$(git rev-parse HEAD)
+results_array["commit_timestamp"]=$(git show -s --format='%ci')
+results_array["commit_message"]=$(git show -s --format='%s')
+results_array["author_name"]=$(git show -s --format='%an')
+results_array["author_email"]=$(git show -s --format='%ae')
+
+results_array["hostname"]=$(hostname)
+results_array["cpu_load_averages"]=$load_avg  # Assuming you've set this before
+
+# 2. Build and compile
+log_with_timestamp "Building"
+output=$( (cd "${PROJECT_ROOT}" && mvn install:install-file "-Dfile=lib\core.jar" -DgroupId="processing.core" -DartifactId=core -Dversion="1.0" -Dpackaging=jar) 2>&1 )
+mvn_exit_code=$?
+log_with_timestamp "$output"
+if [ $mvn_exit_code -ne 0 ]; then
+    results_array["benchmark_status"]="ERROR"
     remove_lockfile
     exit 1
 fi
 
-echo "Load averages before benchmark: $load_avg" | tee -a "${BENCHMARK_RESULTS}"
-(cd "${PROJECT_ROOT}" && mvn exec:java -Dexec.args="--benchmark --runtime $RUNTIME_SECONDS --fps-cap $FPS_CAP") 2>&1 | tee -a "${BENCHMARK_RESULTS}"
-# Check the exit status of the java command, not the tee
-if [ "${PIPESTATUS[0]}" -ne 0 ]; then
-    echo "Running benchmark failed" | tee -a "${BENCHMARK_RESULTS}"
+output=$( (cd "${PROJECT_ROOT}" && mvn compile) 2>&1 )
+mvn_exit_code=$?
+log_with_timestamp "$output"
+if [ $mvn_exit_code -ne 0 ]; then
+    results_array["benchmark_status"]="ERROR"
     remove_lockfile
     exit 1
 fi
 
-# Parse the frame counts and fps
-baseline_frames=$(parse_frame_count "${BASELINE_FILENAME}")
-current_frames=$(parse_frame_count "${BENCHMARK_RESULTS}")
-average_fps=$(parse_average_fps "${BENCHMARK_RESULTS}")
+# Capture the benchmark output
+benchmark_output=$(cd "${PROJECT_ROOT}" && mvn exec:java -Dexec.args="--benchmark --runtime $RUNTIME_SECONDS --fps-cap $FPS_CAP" 2>&1)
+mvn_exit_code=$?
+log_with_timestamp "$benchmark_output"
+if [ $mvn_exit_code -ne 0 ]; then
+    results_array["benchmark_status"]="ERROR"
+    remove_lockfile
+    exit 1
+fi
 
-# Calculate the difference and append to the log
-difference=$((current_frames - baseline_frames))
-percentage_change=$(echo "scale=2; ($current_frames - $baseline_frames) / $baseline_frames * 100" | bc)
-echo "Difference from baseline: $difference frames ($percentage_change%)" | tee -a "${BENCHMARK_RESULTS}"
+# 3. Parse the output for needed info and add to result array
+
+# TODO: move calculations to python? THIS DOESN'T WORK ATM!
+results_array["baseline_frames"]=$(parse_frame_count "$(cat "${BASELINE_FILENAME}")")
+results_array["current_frames"]=$(parse_frame_count "$benchmark_output")
+results_array["draw_average_fps"]=$(parse_average_fps "$benchmark_output")
+
+difference=$((results_array["current_frames"] - results_array["baseline_frames"]))
+percentage_change=$(echo "scale=2; ($difference) / ${results_array["baseline_frames"]} * 100" | bc)
+
+results_array["difference_frames"]=$difference
+results_array["difference_percentage"]=$percentage_change
+
+# 4. Pipe the result array to our python script
+
+for key in "${!results_array[@]}"; do
+    echo "$key=${results_array[$key]}"
+done | ./save_result.py "${BENCHMARK_RESULTS}"
 
 # Send results to discord
-commit_message=$(git log -1 | tail -n1 | awk '{$1=$1};1')  # awk command trims whitespace
-result_msg="\`Benchmark for commit (${commit_message}): ${current_frames} frames, ${average_fps} average FPS, difference from baseline: ${difference} frames (${percentage_change}%)\`"
+result_msg="\`Benchmark for commit (${results_array['commit_message']}): ${results_array['current_frames']} frames, ${results_array['average_fps']} average FPS, difference from baseline: ${results_array['difference']} frames (${results_array['percentage_change']}%)\`"
 send_discord_message "${DISCORD_WEBHOOK}" "${result_msg}"
 
 # Push results
