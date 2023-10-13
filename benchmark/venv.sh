7d1e446546f355b8c433f94d4b744ef2eb92a297
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-13 13:35:57 +0300

    Use return instead of exit

--- benchmark/venv.sh
+++ benchmark/venv.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
+# Exports all environment variables from a file
 # Example usage
 # load_venv "/path/to/.env"
 load_venv() {
     # Check if the argument (file path) was provided
     if [[ -z "$1" ]]; then
         echo "Error: No path provided to load_venv" >&2
-        exit 1
+        return 1
     fi
 
     # Check if the file exists
     if [[ ! -f "$1" ]]; then
         echo "Error: File $1 does not exist" >&2
-        exit 1
+        return 1
     fi
 
     # Check if the file contains any assignment-like lines (e.g., VAR=value)
     if ! grep -Eq '^[a-zA-Z_][a-zA-Z0-9_]*=.*$' "$1"; then
         echo "Error: File $1 does not appear to contain any variables" >&2
-        exit 1
+        return 1
     fi
 
     # Source the file and export the variables
