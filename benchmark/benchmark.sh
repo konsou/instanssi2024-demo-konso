6a253427714541815ead6698df7a18e025701123
Tomi Javanainen <tomi.javanainen@gmail.com>
2023-10-13 14:06:54 +0300

    Use load_venv

--- benchmark/benchmark.sh
+++ benchmark/benchmark.sh
@@ -17,21 +17,19 @@ SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 DOTENV_FILE="${SCRIPT_DIR}/.env"
 RESULT_DIR="${SCRIPT_DIR}/results"
 PROJECT_ROOT="${SCRIPT_DIR%/*}"
-OUTPUT_FILENAME="${RESULT_DIR}/benchmark.log"
+BENCHMARK_RESULTS="${RESULT_DIR}/benchmark.log"
 BASELINE_FILENAME="${RESULT_DIR}/benchmark-baseline.log"
+DETAILED_LOG_DIR="${PROJECT_ROOT}/log"
+DETAILED_LOG="${DETAILED_LOG_DIR}/benchmark-full.log"
 LOCK_FILE="/tmp/konso-demo-benchmark.lock"
 
-# Load configurations from .env
-CONFIG_VARS="$(grep -v '^#' "${DOTENV_FILE}" | xargs)"
-if [ -n "${CONFIG_VARS}" ]; then
-    export "${CONFIG_VARS?}"
-else
-    echo "Error: No configurations found in ${DOTENV_FILE}." | tee >(cat >&2) >> "${OUTPUT_FILENAME}"
-    exit 1
-fi
+source "${SCRIPT_DIR}/venv.sh"
+mkdir -p "${DETAILED_LOG_DIR}" || { echo "Failed to create log dir ${DETAILED_LOG_DIR}" >&2; exit 1; }
+
+{ load_venv "${DOTENV_FILE}" ; } >> "${DETAILED_LOG}" 2>&1 || exit 1
 
 if [ -z "${XVFB_DISPLAY_NUM}" ]; then
-    echo "Error: XVFB_DISPLAY_NUM is not set in ${DOTENV_FILE}." | tee >(cat >&2) >> "${OUTPUT_FILENAME}"
+    echo "Error: XVFB_DISPLAY_NUM is not set in ${DOTENV_FILE}." | tee >(cat >&2) >> "${BENCHMARK_RESULTS}"
     exit 1
 fi
 
@@ -66,41 +64,41 @@ remove_lockfile() {
 }
 
 # Run the benchmark
-echo "--------------------------" >> "${OUTPUT_FILENAME}"
-echo "Benchmark started at $(date +'%Y-%m-%d %H:%M')" >> "${OUTPUT_FILENAME}"
-git log -1 >> "${OUTPUT_FILENAME}"
+echo "--------------------------" >> "${BENCHMARK_RESULTS}"
+echo "Benchmark started at $(date +'%Y-%m-%d %H:%M')" >> "${BENCHMARK_RESULTS}"
+git log -1 >> "${BENCHMARK_RESULTS}"
 
 # Compile
 echo "Building"
-(cd "${PROJECT_ROOT}" && mvn install:install-file "-Dfile=lib\core.jar" -DgroupId="processing.core" -DartifactId=core -Dversion="1.0" -Dpackaging=jar) || echo "Error adding core.jar to maven" | tee -a "${OUTPUT_FILENAME}"
-(cd "${PROJECT_ROOT}" && mvn compile) || echo "Error compiling" | tee -a "${OUTPUT_FILENAME}"
+(cd "${PROJECT_ROOT}" && mvn install:install-file "-Dfile=lib\core.jar" -DgroupId="processing.core" -DartifactId=core -Dversion="1.0" -Dpackaging=jar) || echo "Error adding core.jar to maven" | tee -a "${BENCHMARK_RESULTS}"
+(cd "${PROJECT_ROOT}" && mvn compile) || echo "Error compiling" | tee -a "${BENCHMARK_RESULTS}"
 # Check the exit status of the java command, not the tee
 if [ "${PIPESTATUS[0]}" -ne 0 ]; then
-    echo "Build failed" | tee -a "${OUTPUT_FILENAME}"
+    echo "Build failed" | tee -a "${BENCHMARK_RESULTS}"
     remove_lockfile
     exit 1
 fi
 
-echo "Load averages before benchmark: $load_avg" | tee -a "${OUTPUT_FILENAME}"
-(cd "${PROJECT_ROOT}" && mvn exec:java -Dexec.args="--benchmark --runtime $RUNTIME_SECONDS --fps-cap $FPS_CAP") 2>&1 | tee -a "${OUTPUT_FILENAME}"
+echo "Load averages before benchmark: $load_avg" | tee -a "${BENCHMARK_RESULTS}"
+(cd "${PROJECT_ROOT}" && mvn exec:java -Dexec.args="--benchmark --runtime $RUNTIME_SECONDS --fps-cap $FPS_CAP") 2>&1 | tee -a "${BENCHMARK_RESULTS}"
 # Check the exit status of the java command, not the tee
 if [ "${PIPESTATUS[0]}" -ne 0 ]; then
-    echo "Running benchmark failed" | tee -a "${OUTPUT_FILENAME}"
+    echo "Running benchmark failed" | tee -a "${BENCHMARK_RESULTS}"
     remove_lockfile
     exit 1
 fi
 
 # Parse the frame counts
 baseline_frames=$(parse_frame_count "${BASELINE_FILENAME}")
-current_frames=$(parse_frame_count "${OUTPUT_FILENAME}")
+current_frames=$(parse_frame_count "${BENCHMARK_RESULTS}")
 
 # Calculate the difference and append to the log
 difference=$((current_frames - baseline_frames))
 percentage_change=$(echo "scale=2; ($current_frames - $baseline_frames) / $baseline_frames * 100" | bc)
-echo "Difference from baseline: $difference frames ($percentage_change%)" | tee -a "${OUTPUT_FILENAME}"
+echo "Difference from baseline: $difference frames ($percentage_change%)" | tee -a "${BENCHMARK_RESULTS}"
 
 # Push results
-git add "${OUTPUT_FILENAME}"
+git add "${BENCHMARK_RESULTS}"
 git commit -m "Add benchmark info"
 git push
 
